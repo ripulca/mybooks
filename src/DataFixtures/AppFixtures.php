453ae5c24fc8f808c864b4db73bebf4b8292290a
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-12 20:50:14 +0300

    auth+reg+migrations+fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,32 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 0; $i < 21; $i++) {
+            $user = new User();
+            $user->setEmail('user_'.$i.'@example.com');
+            $user->setPassword(password_hash('pwd____'.$i, PASSWORD_DEFAULT));
+            $user->setRoles(['ROLE_USER']);
+            $manager->persist($user);
+            $random=rand(1, 20);
+            for($j=0;$j<$random;$j++){
+                $book = new Book();
+                $book->setName('book_'.$j);
+                $book->setAuthor('author_'.$j);
+                $book->setCover('');
+                $book->setFile('');
+                $book->setUserId($user);
+                $manager->persist($book);
+            }
+        }
 
         $manager->flush();
     }
---
+++ src/Entity/Book.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $author;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $cover;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $file;
+
+    #[ORM\Column(type: 'date', nullable: true)]
+    private $last_reading_date;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'books')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getCover(): ?string
+    {
+        return $this->cover;
+    }
+
+    public function setCover(string $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getLastReadingDate(): ?\DateTimeInterface
+    {
+        return $this->last_reading_date;
+    }
+
+    public function setLastReadingDate(?\DateTimeInterface $last_reading_date): self
+    {
+        $this->last_reading_date = $last_reading_date;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
