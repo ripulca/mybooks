71f483ed2c1e4d18e6ffd6ad5d809e8898349988
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-12 23:51:35 +0300

    /

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/')]
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        $session = new Session();
+        $name = $session->get('email') ?? null;
+        return $this->render('index.html.twig', [
+        ]);
+    }
+}
