e07177532ae5b9cbd80f2038656fcd8687aef6a6
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-24 13:11:17 +0300

    Update SecurityController.php

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response       //логин
     {
         // if ($this->getUser()) {
         //     return $this->redirectToRoute('target_path');
@@ -22,11 +22,11 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);        //вывод страницы авторизации
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+    public function logout(): void      //выход из аккаунта
     {
         $session=new Session();
         $session->clear();
