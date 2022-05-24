a4639872d98ff820a41d0a20485a8a97fbdd9473
ripulca <68591785+ripulca@users.noreply.github.com>
2022-05-24 13:17:17 +0300

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -14,7 +14,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface     //сущность пользователя
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
